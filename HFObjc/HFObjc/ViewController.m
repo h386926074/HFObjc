fb2e00e3a577db03331b1e3a9cf34662cbe8e806
huangfeng <huangfeng@163.com>
2017-09-06 09:01:27 +0800

    RACOBJC

--- HFObjc/HFObjc/ViewController.m
+++ HFObjc/HFObjc/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveObjC.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *racButton;
 
 @end
 
@@ -16,6 +18,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [[self.racButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+        NSLog(@"--rac按钮被点击了---%@",x);
+    }];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
